8325b244bc557c917939bff261b6759b76347968
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-06-07 15:55:39 +0530

    database created

--- server/db.js
+++ server/db.js
@@ -3,5 +3,9 @@ const pool = mysql.createPool({
     host:"localhost",
     user:"root",
     password:"manager",
+    port:3306,
+    connectionlimit:5,
+    database:"cars_db"
 
 })
+module.export = {pool};
